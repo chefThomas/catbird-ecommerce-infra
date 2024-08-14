19b56e158eb1d6506f1bc2b10d95ddabaa5970d5
catbirdfarm <catbirdfarms@macbook-pro.mynetworksettings.com>
2024-08-14 09:47:13 -0400

    add vpc

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket to store the terraform state file remotely"
+  default     = "catbird-terraform-state"
+}
+
+variable "vpc_name" {
+  description = "value of the VPC name"
+  default     = "catbird-vpc"
+}
